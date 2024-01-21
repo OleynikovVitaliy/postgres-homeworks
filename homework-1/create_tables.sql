da0f1645e04bb4a032c2e8592f3ae2cd0b204a4d
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2024-01-21 10:00:59 +0500

    Скрипты внесены

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers_data
+(
+	customer_id VARCHAR (50) PRIMARY KEY,
+	company_name VARCHAR (50) NOT NULL,
+	contact_name VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE employees_data
+(
+	employee_id INT PRIMARY KEY,
+	first_name VARCHAR (50) NOT NULL,
+	last_name VARCHAR (50) NOT NULL,
+	title VARCHAR (50) NOT NULL,
+	birth_date DATE NOT NULL,
+	notes TEXT
+);
+
+CREATE TABLE orders_data
+(
+	order_id INT PRIMARY KEY,
+	customer_id VARCHAR (50) REFERENCES customers_data (customer_id) NOT NULL,
+	employee_id INT REFERENCES employees_data (employee_id) NOT NULL,
+	order_date DATE NOT NULL,
+	ship_city VARCHAR (50) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,38 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+connect = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='Polino4ka!'
+)
+
+
+with connect:
+    with connect.cursor() as cursor:
+        with open('north_data/customers_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cursor.execute("insert into customers_data (customer_id, company_name, contact_name) values (%s, %s, %s)", row)
+
+
+with connect:
+    with connect.cursor() as cursor:
+        with open('north_data/employees_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cursor.execute("insert into employees_data (employee_id, first_name, last_name, title, birth_date, notes) values (%s, %s, %s, %s, %s, %s)", row)
+
+
+with connect:
+    with connect.cursor() as cursor:
+        with open('north_data/orders_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cursor.execute("insert into orders_data (order_id, customer_id, employee_id, order_date, ship_city) values (%s, %s, %s, %s, %s)", row)
+
+connect.commit()
